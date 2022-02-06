e2a300d80a4560076cbb8aad556b0e63f7c5b5b0
Kevin Buck <asllamarguy@me.com>
2022-02-06 10:12:12 -0500

    Initial Commit

---
+++ coolcalc2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coolcalc2022
+//
+//  Created by Kevin Buck on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
